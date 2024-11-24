4459c903c33864de8e27c3386c69ebd17bc34b73
Abiel <97120759+abielinsky@users.noreply.github.com>
2024-11-24 17:43:08 +0000

    installing nuget packagess
    Installing some proper packages to do the 2.	Some type of identity management

--- obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
+++ obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6c395e814ea8be63e498bfedab7dd4b537ca28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf5cfcc507e99a284a029da0ac6639c9e8cdcce")]
 [assembly: System.Reflection.AssemblyProductAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
