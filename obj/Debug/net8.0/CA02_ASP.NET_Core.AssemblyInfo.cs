7a035ba647d22fb5e831f2f4379e370e8b5dcb41
Abiel <97120759+abielinsky@users.noreply.github.com>
2024-11-26 13:40:37 +0000

    Token implementation
    Token implementation for authentication working

--- obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
+++ obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fe15161979601d2dc6cb96d9c1953a6221a0b87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82e8f817af3357dbc65ffa630e1b02892a1e904a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
