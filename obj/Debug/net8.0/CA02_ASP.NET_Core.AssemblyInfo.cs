e2cec73aa00283e083c0d12e373cadf4340920f9
Matthew-Riddell <d00245674@student.dkit.ie>
2024-11-30 18:35:11 +0000

    Updates for Deployment to Azure

--- obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
+++ obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaaff7788cc86730b113edb386a891a1d9882db3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a3ef5db98f4989b94ad2bc952dd3989887d159")]
 [assembly: System.Reflection.AssemblyProductAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
