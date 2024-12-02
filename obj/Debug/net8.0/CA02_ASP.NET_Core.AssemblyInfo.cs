bdcc4180738fd295503dc49d4a39f951b4a67545
Matthew-Riddell <d00245674@student.dkit.ie>
2024-12-02 14:46:32 +0000

    Updates to the Azure connection
    Azure Connection

--- obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
+++ obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a3ef5db98f4989b94ad2bc952dd3989887d159")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a9008a3b24526a58325c11b416bf5355b4b172a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
