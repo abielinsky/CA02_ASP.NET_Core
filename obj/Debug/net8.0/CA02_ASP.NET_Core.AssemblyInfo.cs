ea47e53a2e66307d01ccae3e7e8d73826603f83e
Matthew-Riddell <d00245674@student.dkit.ie>
2024-12-02 19:45:45 +0000

    updated dbconnection string

--- obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
+++ obj/Debug/net8.0/CA02_ASP.NET_Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a9008a3b24526a58325c11b416bf5355b4b172a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c4f6081f73340698761a495643320818417703")]
 [assembly: System.Reflection.AssemblyProductAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CA02_ASP.NET_Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
